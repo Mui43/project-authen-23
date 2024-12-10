43a4520695be7524700fec4400f6a0a879c3271c
tanapat muijeen <tanapat.muijeen@gmail.com>
2024-12-10 09:23:51 +0700

    first commit

---
+++ src/app/dataapi.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataapiService {
+
+  constructor(public http:HttpClient) {
+
+  }
+  registerUser(data:any){
+   return this.http.post('http://127.0.0.1/2-3/Project01/register.php', data);
+  }
+}
